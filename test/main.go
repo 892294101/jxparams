4669dc03f68f7a1405fc5960fffff97644e0571a
892294101 <892294101@qq.com>
2023-12-29 15:50:50 +0800

    add test content

--- test/main.go
+++ test/main.go
@@ -9,6 +9,23 @@ import (
 func main() {
 	p := jxparams.NewParams()
 	p.SetParams("jx.server.listener.port", jxparams.NewConfig().SetDefault("5100"))
+	p.SetParams("jx.server.model")
+	p.SetParams("jx.filesystem.image.path")
+	p.SetParams("jx.filesystem.host")
+	p.SetParams("jx.database.type")
+
+	p.SetParams("jx.database.host")
+
+	p.SetParams("jx.database.port")
+	p.SetParams("jx.database.name")
+	p.SetParams("jx.database.username")
+	p.SetParams("jx.database.password")
+	p.SetParams("jx.database.charset")
+	p.SetParams("jx.database.connect.max.idle")
+	p.SetParams("jx.database.connect.min.idle")
+	p.SetParams("jx.database.connect.max")
+	p.SetParams("jx.redis.address")
+	p.SetParams("jx.redis.password")
 
 	p.SetConfigFile("/Users/kqwang/development/gowork/src/github.com/892294101/jxshop/server/build/conf/shop.conf")
 
